9e8dc8399acdafad1d3a68af5452494f54fb27c6
Ganaa Ravdan <r.chimeg@gmail.com>
2019-01-28 23:07:08 +0000

    Created gulp task to complie sass to css

--- web/themes/custom/company/gulpfile.js
+++ web/themes/custom/company/gulpfile.js
@@ -1,7 +1,8 @@
-const config = require('./gulp/_config');
-const gulp = require('gulp');
-const bs = require('browser-sync').create();
 
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+//const bs = require('browser-sync').create();
+/*
 require('./gulp/styles')(gulp, config, bs);
 require('./gulp/scripts')(gulp, config, bs);
 require('./gulp/svg')(gulp, config, bs);
@@ -9,4 +10,11 @@ require('./gulp/stats')(gulp, config, bs);
 require('./gulp/watch')(gulp, config, bs);
 require('./gulp/lint')(gulp, config);
 require('./gulp/default')(gulp, config);
+*/
 
+gulp.task('sass', function() {
+    return gulp.src('sass/global.scss') // Gets all files ending with .scss in app/scss and children dirs
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'))
+
+});
